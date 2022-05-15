a7571c51894578bfa196873d0225efcea0f2ee68
Satiko <satikoshapy@yahoo.com>
2022-05-15 15:45:14 +0200

    each pokemon has styled types on the sidebar list

--- src/shared/models/pokemon.model.ts
+++ src/shared/models/pokemon.model.ts
@@ -6,6 +6,6 @@ export class Pokemon {
         public id: number,
         public name: string,
         public sprites: Sprite,
-        public types: PokemonType,
+        public types: PokemonType[],
     ) {}
 }
